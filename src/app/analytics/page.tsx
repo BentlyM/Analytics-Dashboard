41d770f759094e8f7c499fe1e96d5f1be49a7b7d
BentlyM <metayerbently@gmail.com>
2024-02-12 21:43:56 -0500

    avg vistors per day functionality

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,13 +1,27 @@
-import { Analytics, analytics } from "@/utils/analytics"
+import AnalyticsDashboard from "@/components/AnalyticsDashboard"
+import { analytics } from "@/utils/analytics"
 
 const Page = async () => {
 
+    const TRACKING_DAYS = 7
 
-    const pageView = await analytics.retrieveDays("pageview" , 2)
+    const pageviews = await analytics.retrieveDays("pageview" , TRACKING_DAYS)
+
+    const totalPageviews = pageviews.reduce((acc, curr) => {
+        return (
+            acc + curr.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!
+            }, 0)
+        )
+    }, 0)
+
+    const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1)
+
+    
 
     return <div className="min-h-screen w-full py-12 flex justify-center items-center">
         <div className="relative w-full max-w-6x1 mx-auto text-white">
-            <AnalyticsDashboard />
+            <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
         </div>
     </div>
 }
